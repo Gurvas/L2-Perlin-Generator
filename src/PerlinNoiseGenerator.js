cd2ce9d3378a0219477dd266ee6e2bd93ee1a20f
Gustav Larsson <gl222pp@student.lnu.se>
2024-09-30 11:09:18 +0200

    Add fields to hold the other classes in generatorclass

--- src/PerlinNoiseGenerator.js
+++ src/PerlinNoiseGenerator.js
@@ -4,11 +4,16 @@ import { NoiseMatrix } from './NoiseMatrix.js'
 import { Gradient } from './Gradient.js'
 
 export class PerlinNoiseGenerator {
+  #seed
+  #noiseMatrix
+  #gradient
+  #interpolation
+  #randomGenerator
 
   constructor(seed = Math.random()) {
-    this.seed = seed
-    this.NoiseMatrix = new NoiseMatrix()
-    this.gradient = new Gradient(seed)
+    this.#seed = seed
+    this.#noiseMatrix = new NoiseMatrix()
+    this.#gradient = new Gradient(seed)
   }
   
   generateNoise() {
