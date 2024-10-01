fa719fe196c8cdf0a81bfdf5261168ca328949b8
Gustav Larsson <gl222pp@student.lnu.se>
2024-10-01 15:02:54 +0200

    Write more code for matrix generator

--- src/NoiseMatrix.js
+++ src/NoiseMatrix.js
@@ -9,12 +9,23 @@ export class NoiseMatrix {
       throw new Error('No width or height selected (should be integers)')
     }
     const matrix = []
-
+    for (let i = 0; i < this.width; i++) {
+      matrix.push(this.createMatrixRow())
+    }
+    return matrix
   }
 
   setHeightAndWidth(height, width) {
     this.#height = height
     this.#width = width
+  }
+
+  createMatrixRow() {
+    const row = []
+    for (let i = 0; i < this.#height; i++) {
+      row.push(this.#generateRandomValue())
+    }
+    return row
   }
 
   #generateRandomValue() {
